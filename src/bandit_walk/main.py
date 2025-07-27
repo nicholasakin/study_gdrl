b8acb589fd55c19c507b35aa0be2b9e2e91a8db8
Akin, Nicholas <nicholasakin@gmail.com>
2025-07-26 19:02:18 -0500

    feat: added number of times to replay env

--- src/bandit_walk/main.py
+++ src/bandit_walk/main.py
@@ -2,23 +2,23 @@ from bandit_walk.env import BanditWalkEnv
 import random
 
 
+num_times = 5
 
-bw = BanditWalkEnv(start_state=1)
-
-
-is_terminal = False
-while not is_terminal:
-
-
-    action = random.choice([0,1])
+for i in range(num_times):
+    print(f"\nNum Times Run: {i}")
+    bw = BanditWalkEnv(start_state=1)
 
 
-    next_state, reward, is_terminal = bw.step(action=action)
-    print(f"I am in state: {next_state} with reward {reward}")
-    print(f"This is terminal {is_terminal}")
+    is_terminal = False
+    while not is_terminal:
 
 
+        action = random.choice([0,1])
 
+
+        next_state, reward, is_terminal = bw.step(action=action)
+        print(f"I am in state: {next_state} with reward {reward}")
+        print(f"This is terminal {is_terminal}")
 
 
 
